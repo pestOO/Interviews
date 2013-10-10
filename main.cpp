e496cf8af2bfad492075115d23f4b8ffb673c9d3
pestOO <EliseyZamahov@gmail.com>
2013-10-10 21:18:51 +0400

    *add test logics

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include "CDArray.h"
+#define N 700
 
 using namespace std;
 
 int main()
     {
-    cout << "Hello World!" << endl;
+    cout << "In CDArray will be added " << N << " elements!" << endl;
+    cout << "Inpute minimum value!" << endl;
+    int min;
+    cin >> min >> endl;
+    cout << "Inpute maximum value!" << endl;
+    int max;
+    cin >> max >> endl;
+    CDArray<int> array;
+
+    for (int i = 0; i < N; ++i)
+        array.Add (rand ());
+    cout << "Array values : " << endl;
+    for (int i = 0; i < array.Number (); ++i)
+        cout << array[i] << ' ';
+
+    for (int i = 0; i < N/2; ++i)
+        array.Delete (1);
+    cout << "New array values : " << endl;
+    for (int i = 0; i < array.Number(); ++i)
+        cout << array[i] << ' ';
+
+    std::sort(array.Ptr(), array.Ptr() + array.Number());
+    cout << "Sorted array values : " << endl;
+    for (int i = 0; i < array.Number(); ++i)
+        cout << array[i] << ' ';
     return 0;
     }
 
