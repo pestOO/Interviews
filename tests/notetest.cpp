c560b74715efd8495a9007629b353e7b1c248954
Elisey Zamakhov <Elisey.Zamakhov@tomtom.com>
2019-02-23 16:59:02 +0100

    Update Note, add simple unit tests

---
+++ tests/notetest.cpp
@@ -0,0 +1,29 @@
+#include <Note.hpp>
+#include <iostream>
+
+void TestNoteConstruction()
+{
+    Note note;
+    assert(note.getTitle().empty());
+    assert(note.getText().empty());
+    assert(note.getTags().empty());
+}
+
+void TestNoteSetGet()
+{
+    Note note;
+    note.setTitle("Title");
+    note.setText("Text");
+    note.setTags({"Tag1", "Tag2"});
+
+    assert(note.getTitle() == "Title");
+    assert(note.getText() == "Text");
+    assert(note.getTags() == Note::TTagContainer({"Tag1", "Tag2"}));
+}
+
+void TestNote()
+{
+    TestNoteConstruction();
+    TestNoteSetGet();
+    std::cout << __FUNCTION__ << " finished" << std::endl;
+}
